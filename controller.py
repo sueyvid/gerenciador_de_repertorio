16927be3846651a691c662fbb55cb5afe79a8887
Sueyvid <sueyvidr8@gmail.com>
2023-01-03 00:27:55 -0300

    adicionado treeview

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from view import View
 from model import Model
+import tkinter as tk
 
 
 class Controller:
@@ -8,6 +9,7 @@ class Controller:
         self.model = model
         self.nome_do_arquivo = 'banco_de_dados.csv'
         self.configs()
+        self.recupera_dados()
 
     def configs(self):
         bt_Adicionar = self.view.bAdicionar
@@ -15,12 +17,23 @@ class Controller:
         bt_Limpar = self.view.bLimpar
         bt_Limpar['command'] = self.limpar
 
+    def recupera_dados(self):
+        musicas = self.model.ler_csv(self.nome_do_arquivo)
+        tv = self.view.tv
+        for musica in musicas[1:]:
+            tv.insert('', tk.END, values=(musica[0], musica[1], musica[2], musica[3]))
+
+    def adicionar_tv(self, musica):
+        tv = self.view.tv
+        tv.insert('', tk.END, values=(musica[0], musica[1], musica[2], musica[3]))
+
     def adicionar(self):
         nome = self.view.nomeVar.get()
         artista = self.view.artistaVar.get()
         tom = self.view.tomVar.get()
         ritmo = self.view.ritmoVar.get()
         self.model.adicionar_csv(self.nome_do_arquivo, [nome, artista, tom, ritmo])
+        self.adicionar_tv([nome, artista, tom, ritmo])
         self.view.nomeVar.set('')
         self.view.artistaVar.set('')
         self.view.tomVar.set('')
--- view.py
+++ view.py
@@ -1,55 +1,64 @@
 import tkinter as tk
+from tkinter import ttk
 
 
 class View(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Gerenciador de Repertórios")
-        self.iniciar()
         self.colunas = ['Nome', 'Artísta', 'Tom', 'Ritmo']
+        self.iniciar()
 
     def iniciar(self):
         tamLetra = 20
         area_de_trabalho = tk.Frame(self)
-        tTituloPagina = tk.Label(area_de_trabalho, text="Nova Música", font=('', tamLetra))
-
         area_de_trabalho.pack()
-        tTituloPagina.grid(row=0, column=0)
 
-        entradas = tk.Frame(area_de_trabalho)
+        area_de_entrada = tk.Frame(area_de_trabalho)
+        area_de_entrada.grid(row=0, column=0)
+
+        tTituloPagina = tk.Label(area_de_entrada, text="Nova Música", font=('', tamLetra))
+        tTituloPagina.grid(row=0, column=0, sticky='WE')
+        
+        entradas = tk.Frame(area_de_entrada)
         entradas.grid(row=1, column=0)
 
         self.nomeVar = tk.StringVar()
         tNome = tk.Label(entradas, text="Título", font=('', tamLetra))
         eNome = tk.Entry(entradas, font=('', tamLetra), textvariable=self.nomeVar)
-        tNome.grid(row=0, column=0)
-        eNome.grid(row=0, column=1)
+        tNome.grid(row=1, column=0)
+        eNome.grid(row=1, column=1)
 
         self.artistaVar = tk.StringVar()
         tArtista = tk.Label(entradas, text="Artísta", font=('', tamLetra))
         eArtista = tk.Entry(entradas, font=('', tamLetra), textvariable=self.artistaVar)
-        tArtista.grid(row=1, column=0)
-        eArtista.grid(row=1, column=1)
+        tArtista.grid(row=2, column=0)
+        eArtista.grid(row=2, column=1)
 
         self.tomVar = tk.StringVar()
         tTom = tk.Label(entradas, text="Tom", font=('', tamLetra))
         eTom = tk.Entry(entradas, font=('', tamLetra), textvariable=self.tomVar)
-        tTom.grid(row=2, column=0)
-        eTom.grid(row=2, column=1)
+        tTom.grid(row=3, column=0)
+        eTom.grid(row=3, column=1)
 
         self.ritmoVar = tk.StringVar()
         tRitmo = tk.Label(entradas, text="Ritmo", font=('', tamLetra))
         eRitmo = tk.Entry(entradas, font=('', tamLetra), textvariable=self.ritmoVar)
-        tRitmo.grid(row=3, column=0)
-        eRitmo.grid(row=3, column=1)
+        tRitmo.grid(row=4, column=0)
+        eRitmo.grid(row=4, column=1)
 
-        botoes = tk.Frame(area_de_trabalho)
+        botoes = tk.Frame(area_de_entrada)
         self.bAdicionar = tk.Button(botoes, text='Adicionar', font=('', tamLetra))
         self.bLimpar = tk.Button(botoes, text='Limpar', font=('', tamLetra))
 
         botoes.grid(row=2, column=0)
         self.bAdicionar.grid(row=0, column=0)
         self.bLimpar.grid(row=0, column=1)
+
+        self.tv = ttk.Treeview(area_de_trabalho, columns=self.colunas, show='headings')
+        for i in self.colunas:
+            self.tv.heading(i, text=i)
+        self.tv.grid(row=0, column=1, sticky='NSWE')
 
 if __name__ == "__main__":
     tela = View()
