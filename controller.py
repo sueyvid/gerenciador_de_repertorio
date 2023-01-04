c75b91364bdb6e36868474108fae07bb5bab8e75
Sueyvid <sueyvidr8@gmail.com>
2023-01-04 02:24:14 -0300

    dados temporários

--- controller.py
+++ controller.py
@@ -4,8 +4,6 @@ import tkinter as tk
 from tkinter import filedialog as fd
 
 
-#TODO: Opção de escolher base de dados
-#TODO: Fazer cópia da base de dados
 #TODO: Opção de salvar base de dados
 #TODO: Treeview do repertório
 
@@ -14,10 +12,8 @@ class Controller:
     def __init__(self, view, model):
         self.view = view
         self.model = model
-        # self.nome_do_arquivo = '../gerenciador_repertorio/dados/banco_de_dados.csv'
         self.nome_do_arquivo = ''
         self.config_botoes()
-        # self.recupera_dados()
 
     def config_botoes(self):
         bt_Adicionar = self.view.bAdicionar
@@ -48,16 +44,21 @@ class Controller:
 
     def recupera_dados(self):
         musicas = self.model.ler_csv(self.nome_do_arquivo)
+        nome_do_arquivo = 'temp.csv'
+        self.limpar()
         tv = self.view.tv
         for musica in musicas[1:]:
-            tv.insert('', tk.END, values=(musica[0], musica[1], musica[2], musica[3]))    
+            nome, artista, tom, ritmo = musica
+            self.model.adicionar_csv(nome_do_arquivo, [nome, artista, tom, ritmo])
+            tv.insert('', tk.END, values=(nome, artista, tom, ritmo))
 
     def adicionar(self):
+        nome_do_arquivo = 'temp.csv'
         nome = self.view.nomeVar.get()
         artista = self.view.artistaVar.get()
         tom = self.view.tomVar.get()
         ritmo = self.view.ritmoVar.get()
-        self.model.adicionar_csv(self.nome_do_arquivo, [nome, artista, tom, ritmo])
+        self.model.adicionar_csv(nome_do_arquivo, [nome, artista, tom, ritmo])
         tv = self.view.tv
         tv.insert('', tk.END, values=(nome, artista, tom, ritmo))
         self.view.nomeVar.set('')
@@ -66,15 +67,17 @@ class Controller:
         self.view.ritmoVar.set('')
 
     def remover(self):
+        nome_do_arquivo = 'temp.csv'
         tv = self.view.tv
         for selected_item in tv.selection():
             index = tv.index(selected_item)
-            self.model.remover_linha_csv(self.nome_do_arquivo, index+1)
+            self.model.remover_linha_csv(nome_do_arquivo, index+1)
             tv.delete(selected_item)
 
     def limpar(self):
+        nome_do_arquivo = 'temp.csv'
         col = self.view.colunas
-        self.model.criar_csv(self.nome_do_arquivo, col)
+        self.model.criar_csv(nome_do_arquivo, col)
 
 if __name__ == '__main__':
     v = View()
