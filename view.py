e4ab099be9ca371ca9eb766913c141d2f041c208
Sueyvid <sueyvidr8@gmail.com>
2023-01-05 04:06:16 -0300

    tkinter temático

--- view.py
+++ view.py
@@ -12,64 +12,67 @@ class View(tk.Tk):
     def iniciar(self):
         # Area de trabalho
         tamLetra = 20
-        area_de_trabalho = tk.Frame(self)
+        area_de_trabalho = ttk.Frame(self)
         area_de_trabalho.pack()
 
         # Area de entrada
-        area_de_entrada = tk.Frame(area_de_trabalho)
+        area_de_entrada = ttk.Frame(area_de_trabalho)
         area_de_entrada.grid(row=0, column=0)
 
-        tTituloEntrada = tk.Label(area_de_entrada, text="Nova Música", font=('', tamLetra))
+        tTituloEntrada = ttk.Label(area_de_entrada, text="Nova Música")
         tTituloEntrada.grid(row=0, column=0, sticky='WE')
         
-        entradas = tk.Frame(area_de_entrada)
+        entradas = ttk.Frame(area_de_entrada)
         entradas.grid(row=1, column=0)
 
         self.nomeVar = tk.StringVar()
-        tNome = tk.Label(entradas, text="Título", font=('', tamLetra))
-        eNome = tk.Entry(entradas, font=('', tamLetra), textvariable=self.nomeVar)
+        tNome = ttk.Label(entradas, text="Título")
+        eNome = ttk.Entry(entradas, textvariable=self.nomeVar)
         tNome.grid(row=1, column=0)
         eNome.grid(row=1, column=1)
 
         self.artistaVar = tk.StringVar()
-        tArtista = tk.Label(entradas, text="Artísta", font=('', tamLetra))
-        eArtista = tk.Entry(entradas, font=('', tamLetra), textvariable=self.artistaVar)
+        tArtista = ttk.Label(entradas, text="Artísta")
+        eArtista = ttk.Entry(entradas, textvariable=self.artistaVar)
         tArtista.grid(row=2, column=0)
         eArtista.grid(row=2, column=1)
 
         self.tomVar = tk.StringVar()
-        tTom = tk.Label(entradas, text="Tom", font=('', tamLetra))
-        eTom = tk.Entry(entradas, font=('', tamLetra), textvariable=self.tomVar)
+        tTom = ttk.Label(entradas, text="Tom")
+        eTom = ttk.Entry(entradas, textvariable=self.tomVar)
         tTom.grid(row=3, column=0)
         eTom.grid(row=3, column=1)
 
         self.ritmoVar = tk.StringVar()
-        tRitmo = tk.Label(entradas, text="Ritmo", font=('', tamLetra))
-        eRitmo = tk.Entry(entradas, font=('', tamLetra), textvariable=self.ritmoVar)
+        tRitmo = ttk.Label(entradas, text="Ritmo")
+        eRitmo = ttk.Entry(entradas, textvariable=self.ritmoVar)
         tRitmo.grid(row=4, column=0)
         eRitmo.grid(row=4, column=1)
 
-        botoes = tk.Frame(area_de_entrada)
+        botoes = ttk.Frame(area_de_entrada)
         botoes.grid(row=2, column=0)
-        self.bAdicionar = tk.Button(botoes, text='Adicionar', font=('', tamLetra))
+        self.bAdicionar = ttk.Button(botoes, text='Adicionar')
         self.bAdicionar.grid(row=0, column=0)
-        self.bRemover = tk.Button(botoes, text='Remover', font=('', tamLetra))
+        self.bRemover = ttk.Button(botoes, text='Remover')
         self.bRemover.grid(row=0, column=1)
-        self.bLimpar = tk.Button(botoes, text='Limpar', font=('', tamLetra))
+        self.bLimpar = ttk.Button(botoes, text='Limpar')
         self.bLimpar.grid(row=0, column=2)
 
         # Área de arquivo
-        area_de_arquivo = tk.Frame(area_de_trabalho)
+        area_de_arquivo = ttk.Frame(area_de_trabalho)
         area_de_arquivo.grid(row=0, column=1)
-        tTituloArquivo = tk.Label(area_de_arquivo, text="Escolher Arquivo", font=('', tamLetra))
+        tTituloArquivo = ttk.Label(area_de_arquivo, text="Escolher Arquivo")
         tTituloArquivo.grid(row=0, column=0)
         self.arquivoVar = tk.StringVar(value='nome do arquivo')
-        nome_do_arquivo = tk.Label(area_de_arquivo, textvariable=self.arquivoVar, font=('', 15))
+        nome_do_arquivo = ttk.Label(area_de_arquivo, textvariable=self.arquivoVar)
         nome_do_arquivo.grid(row=1, column=0)
-        self.bSelecionar = tk.Button(area_de_arquivo, text='Selecionar Arquivo', font=('', tamLetra))
+        self.bSelecionar = ttk.Button(area_de_arquivo, text='Selecionar Arquivo')
         self.bSelecionar.grid(row=2, column=0)
         self.bSalvar = ttk.Button(area_de_arquivo, text='Salvar Arquivo')
         self.bSalvar.grid(row=3, column=0)
+        s = ttk.Style()
+        # print(s.theme_names())
+        s.theme_use('clam')
         self.bSalvar.state(['disabled'])
 
         # Área treeview
