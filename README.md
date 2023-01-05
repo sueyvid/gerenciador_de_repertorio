838091343e919b9b983a34119c3957aa10c31837
Sueyvid <sueyvidr8@gmail.com>
2023-01-05 13:55:49 -0300

    métodos OS e organização do controller

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+Adições atuais:
 - TODO: Treeview do repertório
 - TODO: Tratar erro de arquivo inadequado na opção de selecionar arquivo,
 erro: ValueError: not enough values to unpack (expected 4, got 2)
+- TODO: Opção de recuperar os dados perdidos ao abrir o programa
+- TODO: Opção de salvar ao fechar o programa
+
+Adições futuras:
+- TODO: Aba de configurações
+- TODO: ...
+- TODO: Configuração do tamanho da letra
--- controller.py
+++ controller.py
@@ -2,106 +2,101 @@ from view import View
 from model import Model
 import tkinter as tk
 from tkinter import filedialog as fd
+import os
 
 
 class Controller:
     def __init__(self, view, model):
         self.view = view
         self.model = model
-        self.nome_do_arquivo = ''
+        self.config_os()
         self.config_botoes()
 
+    def config_os(self):
+        self.nome_do_arquivo = ''
+        self.arquivo_raiz = ''
+        self.filetypes = (('csv files', '*.csv'), ('All files', '*.*'))
+
+        pasta_raiz = os.getcwd()
+        self.arquivo_temp = f'{pasta_raiz}/temp.csv'
+        self.local_do_arquivo = f'{pasta_raiz}/dados'
+        if not 'dados' in os.listdir(pasta_raiz):
+            os.mkdir(self.local_do_arquivo)
+
     def config_botoes(self):
-        bt_Adicionar = self.view.bAdicionar
-        bt_Adicionar['command'] = self.adicionar
-        bt_Remover = self.view.bRemover
-        bt_Remover['command'] = self.remover
-        bt_Limpar = self.view.bLimpar
-        bt_Limpar['command'] = self.limpar
-        bt_Selecionar = self.view.bSelecionar
-        bt_Selecionar['command'] = self.selecionar
-        bt_Salvar = self.view.bSalvar
-        bt_Salvar['command'] = self.salvar
+        self.view.bAdicionar['command'] = self.adicionar
+        self.view.bRemover['command'] = self.remover
+        self.view.bLimpar['command'] = self.limpar
+        self.view.bSelecionar['command'] = self.selecionar
+        self.view.bSalvar['command'] = self.salvar
 
     def salvar(self):
-        filetypes = (
-            ('csv files', '*.csv'),
-            ('All files', '*.*')
-        )
-
+        '''Abre uma caixa de diálogo para salvar o arquivo csv'''
         localfile = fd.asksaveasfilename(
             confirmoverwrite=True,
-            filetypes=filetypes,
-            initialdir='/Desktop')
+            filetypes=self.filetypes,
+            initialdir=self.local_do_arquivo)
 
-        local = self.nome_do_arquivo.split('/')
-        temp = ''
-        for i in local[:-2]:
-            temp += f'{i}/'
-        temp += 'temp.csv'
-        # print(self.model.ler_csv(s))
-
-        conteudo = self.model.ler_csv(temp)
+        # TODO: lê um arquivo, escreve outro e adiciona na treeview (fazer função no model)
+        conteudo = self.model.ler_csv(self.arquivo_temp)
         col = self.view.colunas
-        self.model.criar_csv(self.nome_do_arquivo, col)
+        self.model.criar_csv(self.arquivo_raiz, col)
         for linha in conteudo[1:]:
-            self.model.adicionar_csv(self.nome_do_arquivo, linha)
-
+            self.model.adicionar_csv(self.arquivo_raiz, linha)
 
     def selecionar(self):
-        filetypes = (
-            ('csv files', '*.csv'),
-            ('All files', '*.*')
-        )
-
+        '''Abre uma caixa de diálogo para selecionar um arquivo csv com os dados'''
         localfile = fd.askopenfilename(
             title='Open a file',
-            initialdir='/Desktop',
-            filetypes=filetypes)
+            initialdir=self.local_do_arquivo,
+            filetypes=self.filetypes)
 
         if localfile:
-            filename = localfile.split('/')[-1]
-            self.nome_do_arquivo = f'{localfile}'
+            self.nome_do_arquivo = localfile.split('/')[-1]
+            self.arquivo_raiz = localfile
             self.recupera_dados()
-            self.view.arquivoVar.set(value=f'nome do arquivo: {filename}')
+            self.view.arquivoVar.set(value=f'nome do arquivo: {self.nome_do_arquivo}')
             self.view.bSalvar['state'] = tk.NORMAL
 
     def recupera_dados(self):
-        musicas = self.model.ler_csv(self.nome_do_arquivo)
-        nome_do_arquivo = 'temp.csv'
+        '''limpa a treeview e o arquivo temporário,
+        depois adiciona os dados no arquivo e na treeview'''
+        musicas = self.model.ler_csv(self.arquivo_raiz)
         self.limpar()
+        self.adicionar_musicas(musicas[1:])
+
+    def adicionar_musicas(self, musicas):
         tv = self.view.tv
-        for musica in musicas[1:]:
+        for musica in musicas:
             nome, artista, tom, ritmo = musica
-            self.model.adicionar_csv(nome_do_arquivo, [nome, artista, tom, ritmo])
+            self.model.adicionar_csv(self.arquivo_temp, [nome, artista, tom, ritmo])
             tv.insert('', tk.END, values=(nome, artista, tom, ritmo))
 
     def adicionar(self):
-        nome_do_arquivo = 'temp.csv'
-        nome = self.view.nomeVar.get()
-        artista = self.view.artistaVar.get()
-        tom = self.view.tomVar.get()
-        ritmo = self.view.ritmoVar.get()
-        self.model.adicionar_csv(nome_do_arquivo, [nome, artista, tom, ritmo])
-        tv = self.view.tv
-        tv.insert('', tk.END, values=(nome, artista, tom, ritmo))
-        self.view.nomeVar.set('')
-        self.view.artistaVar.set('')
-        self.view.tomVar.set('')
-        self.view.ritmoVar.set('')
+        '''adiciona os dados a partir das entradas'''
+        nome = self.get_strVar(self.view.nomeVar)
+        artista = self.get_strVar(self.view.artistaVar)
+        tom = self.get_strVar(self.view.tomVar)
+        ritmo = self.get_strVar(self.view.ritmoVar)
+        self.adicionar_musicas([[nome, artista, tom, ritmo]])
+    
+    def get_strVar(self, variavel):
+        texto = variavel.get()
+        variavel.set('')
+        return texto
 
     def remover(self):
-        nome_do_arquivo = 'temp.csv'
+        '''remove dado, selecionado pelo usuário, do arquivo e da treeview'''
         tv = self.view.tv
         for selected_item in tv.selection():
             index = tv.index(selected_item)
-            self.model.remover_linha_csv(nome_do_arquivo, index+1)
+            self.model.remover_linha_csv(self.arquivo_temp, index+1)
             tv.delete(selected_item)
 
     def limpar(self):
-        nome_do_arquivo = 'temp.csv'
+        '''reseta o arquivo csv e apaga todos os dados da treeview'''
         col = self.view.colunas
-        self.model.criar_csv(nome_do_arquivo, col)
+        self.model.criar_csv(self.arquivo_temp, col)
         tv = self.view.tv
         for i in tv.get_children():
             tv.delete(i)
